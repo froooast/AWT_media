dcc401d60fde435eaa74eb41d9b79ae892bcacba
Sebastian Frost <sebastian.a.frost@gmail.com>
2019-02-06 19:22:15 +0100

    failure in contract

--- contracts/MediaFactory.sol
+++ contracts/MediaFactory.sol
@@ -10,450 +10,309 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract MediaFactory is Ownable{
 
-  // Initialize the safe math data types.
-  using SafeMath for uint256;
+    // Initialize the safe math data types.
+    using SafeMath for uint256;
 
-  struct Media {
-    string preview;
-    string title;
-    // we store adaption sets in a mapping because thats easier 
-    // https://ethereum.stackexchange.com/questions/12611/solidity-filling-a-struct-array-containing-itself-an-array
-    uint nonce;
-  }
+    struct Media {
+        string periodID;
+        string mediaPresentationDuration;
+        string maxSegmentDuration;
+        string minBufferTime;
+        string profiles;
+        string baseURL;
+        uint numAdaptionSets;
+        // we store adaption sets in a mapping because thats easier 
+        // https://ethereum.stackexchange.com/questions/12611/solidity-filling-a-struct-array-containing-itself-an-array
+        uint nonce;
+     //   AdaptionSet[] adaptionSets;
+        mapping(uint => AdaptionSet) adaptionSets;
+    }
 
-  struct Period {
-    string baseUrl;
-    string duration;
-  }
+    struct AdaptionSet {
+        string mimeType;
+        uint maxWidth;
+        uint maxHeight;
+        string maxFrameRate;
+        uint numRepresentation;
+ //       mapping(uint => Representation) representationSets;
+    }
+  /*
+    struct Representation {
+        string id;
+        uint bandwidth;
+        string codecs;
+        uint width;
+        uint height;
+        string frameRate;
+        string audioSamplingRate;
+    }
 
-  struct AdaptionSet {
-    bool segmentAlignment;
-    uint maxWidth;
-    uint maxHeight;
-    uint maxFrameRate;
-    string par;
-    string lang;
-  }
+    struct SegmentTemplate {
+        string timescale;
+        string initialization;
+        string media;
+        uint startNumber;
+        uint duration;
+        uint presentationTimeOffset;
 
-  struct RepresentationSet {
-    string mimeType;
-    string codecs;
-    string sar;
-    uint startWithSAP;
-    uint bandwidth;
-    uint timescale;
-    uint duration;
-    bytes32[] SegmentURL;
-  }
+    }
 
+    struct AudioChannelConfiguration {
+        string schemeIdUri;
+        uint value;
+    } */
+     /*
+    * Global Variables
+    */
+
+    uint mediaNonce = 1; // Used to generate unique hash values for media objects and all related objects as well.
+    mapping (bytes32 => Media) public mediasMapping;
+   
+    event MediaCreated (
+      bytes32 indexed mediaHash,
+      string baseURL
+    );
+
+    //public function to create a MPD
+    function createMedia(
+        string _periodID,
+        string _mediaPresentationDuration,
+        string _maxSegmentDuration,
+        string _minBufferTime,
+        string _profiles,
+        string _baseURL
+    )
+      public
+      
+    {
+        _createMedia(
+            _periodID,
+            _mediaPresentationDuration,
+            _maxSegmentDuration,
+            _minBufferTime,
+            _profiles,
+            _baseURL
+        );
+    }
+
+     //public function to add a adaptionSet to MPD
+    function addAdaptionSetToMedia(
+        bytes32 _mediaHash,
+        string _mimeType,
+        uint _maxWidth,
+        uint _maxHeight,
+        string _maxFrameRate
+    ) public {
+        _addAdaptionSetToMedia(
+            _mediaHash,
+            _mimeType,
+            _maxWidth,
+            _maxHeight,
+            _maxFrameRate
+        );
+    }
     /*
-   * Global Variables
-   */
-
-  uint mediaNonce = 1; // Used to generate unique hash values for media objects and all related objects as well.
-  mapping (bytes32 => Media) public mediasMapping;
-  mapping (bytes32 => Period) public periodsMapping;
-  mapping (bytes32 => AdaptionSet) public adationsMapping;
-  mapping (bytes32 => RepresentationSet) public representationsMapping;
-
-  event MediaCreated (
-    bytes32 indexed mediaHash,
-    string baseURL
-  );
-
-  event PeriodCreated (
-    bytes32 indexed periodHash
-  );
-
-  event AdaptionSetCreated (
-    bytes32 indexed adaptionSetHash
-  );
-
-  event RepresentationSetCreated (
-    bytes32 indexed representationHash
-  );
-
-  function createMedia(
-    
-    string _preview,
-    string _title
-  )
-    public
-  {
-    _createMedia(
-      _preview,
-      _title
-    );
-  }
-
-   function createPeriod(
-    
-    string _baseUrl,
-    string _duration
-  )
-    public
-  {
-    _createPeriod(
-      _baseUrl,
-      _duration
-    );
-  }
-
-  function createAdaptionSet(
-    
-    bool _segmentAlignment,
-    uint _maxWidth,
-    uint _maxHeight,
-    uint _maxFrameRate,
-    string _par,
-    string _lang
-  )
-    public
-  {
-    _createAdaptionSet(
-      _segmentAlignment,
-      _maxWidth,
-      _maxHeight,
-      _maxFrameRate,
-      _par,
-      _lang
-    );
-  }
-
-  function createRepresentationSet(
-    string _mimeType,
-    string _codecs,
-    string _sar,
-    uint _startWithSAP,
-    uint _bandwidth,
-    uint _timescale,
-    uint _duration,
-    bytes32[] _SegmentURL,
-    uint _representationCount,
-    uint _SegmentURLCount
-  )
-    public
-  {
-    _createRepresentationSet(
-      _mimeType,
-      _codecs,
-      _sar,
-      _startWithSAP,
-      _bandwidth,
-      _timescale,
-      _duration,
-      _SegmentURL,
-      _representationCount,
-      _SegmentURLCount
-    );
-  }
-
-  function _getMediaHash(
-    string _preview,
-    string _title,
-    uint _nonce
-  )
-    private
-    pure
-    returns (bytes32)
-  {
-    return keccak256(
-      abi.encodePacked(
-        _preview,
-        _title,
-        _nonce
-      )
-    );
-  }
-
-  function _getPeriodHash(
-      string _baseUrl,
-      string _duration
+    //public function to add a representation to a adaptionSet
+    function addRepresentationToAdaptionSet(
+        string _id,
+        uint _bandwidth,
+        string _codecs,
+        uint _width,
+        uint _height,
+        string _frameRate,
+        bytes32 _mediaHash,
+        uint _numAdaptionSets,
+        string _audioSamplingRate
+    ) public {
+        _addRepresentationToAdaptionSet(
+            _id,
+            _bandwidth,
+            _codecs,
+            _width,
+            _height,
+            _frameRate,
+            _mediaHash,
+            _numAdaptionSets,
+            _audioSamplingRate
+        );
+    }
+*/
+    //create a unique mediaHash
+    function _getMediaHash(
+        string _periodID,
+        string _mediaPresentationDuration,
+        string _maxSegmentDuration,
+        string _minBufferTime,
+        string _profiles,
+        string _baseURL,
+        uint _nonce
     )
-      private
+      public
       pure
       returns (bytes32)
     {
-      return keccak256(
-        abi.encodePacked(
-              _baseUrl,
-              _duration
-        )
-      );
+        return keccak256(
+            abi.encodePacked(
+              _periodID,
+              _mediaPresentationDuration,
+              _maxSegmentDuration,
+              _minBufferTime,
+              _profiles,
+              _baseURL,
+              _nonce
+            )
+        );
     }
 
-  function _getAdaptionHash(
-      bool _segmentAlignment,
-      uint _maxWidth,
-      uint _maxHeight,
-      uint _maxFrameRate,
-      string _par,
-      string _lang
-    )
-      private
-      pure
-      returns (bytes32)
-    {
-      return keccak256(
-        abi.encodePacked(
-              _segmentAlignment,
-              _segmentAlignment,
-              _maxWidth,
-              _maxHeight,
-              _maxFrameRate,
-              _par,
-              _lang
-        )
-      );
-    }
 
-    function _getRepresentationHash(
-      string _mimeType,
-      string _codecs,
-      string _sar,
-      uint _startWithSAP,
-      uint _bandwidth,
-      uint _timescale,
-      uint _duration
-    )
-      private
-      pure
-      returns (bytes32)
-    {
-      return keccak256(
-        abi.encodePacked(
-              _mimeType,
-              _codecs,
-              _sar,
-              _startWithSAP,
-              _bandwidth,
-              _timescale,
-              _duration
-        )
-      );
-    }  
-
- /**
-   * @dev Creates a media and stores it in the contract.
-   * @dev A hash of the media is used as an unique key.
- */
-  function _createMedia(
-    string _preview,
-    string _title
-  )
-    private
-  {
-    // Create the new media by the provided values.
-    Media memory media = Media({
-        preview: _preview,
-        title: _title,
-        nonce: mediaNonce
-    });
-
-    // Generate the unique hash to store the permit.
-    bytes32 mediaHash = _getMediaHash(
-      media.preview,
-      media.title,
-      media.nonce
-    );
-
-    mediasMapping[mediaHash] = media;
-
-    emit MediaCreated(
-      mediaHash,
-      _title
-    );
-
-    // Increase the permit nonce to get continuously unique hash values.
-    mediaNonce = mediaNonce.add(1);
-  }
-
-
-  function getMedia(bytes32 _mediaHash)
+    // public function to get a MPD
+    function getMedia(bytes32 _mediaHash)
     public
     view
-    returns (string, string)
+    returns (string, string, string, string, string, string, uint)
   {
   
     // Build a tuple with the attributes of the specimen object.
-    return (
-      mediasMapping[_mediaHash].preview,
-      mediasMapping[_mediaHash].title
-    );
-  }
+        return (
+          mediasMapping[_mediaHash].periodID,
+          mediasMapping[_mediaHash].mediaPresentationDuration,
+          mediasMapping[_mediaHash].maxSegmentDuration,
+          mediasMapping[_mediaHash].minBufferTime,
+          mediasMapping[_mediaHash].profiles,
+          mediasMapping[_mediaHash].baseURL,
+          mediasMapping[_mediaHash].numAdaptionSets
+        );
+    }
 
-  function _createPeriod(
-    string _baseUrl,
-    string _duration
-  )
+    // public function to get a AdaptionSet out of a MPD
+    function getAdaptionSet(bytes32 _mediaHash, uint numAdaptionSet)
+    public
+    view
+    returns (string, uint, uint, string, uint)
+    {
+        return (
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].mimeType,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].maxWidth,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].maxHeight,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].maxFrameRate,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].numRepresentation
+        );
+    }
+    /*
+    // public function to get a AdaptionSet out of a MPD
+    function getRepresentationSet(bytes32 _mediaHash, uint numAdaptionSet, uint numRepresentationSet)
+    public
+    view
+    returns (string, uint, string, uint, uint, string)
+    {
+        return (
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].representationSets[numRepresentationSet].id,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].representationSets[numRepresentationSet].bandwidth,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].representationSets[numRepresentationSet].codecs,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].representationSets[numRepresentationSet].width,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].representationSets[numRepresentationSet].height,
+            mediasMapping[_mediaHash].adaptionSets[numAdaptionSet].representationSets[numRepresentationSet].audioSamplingRate
+        );
+    }
+  */
+  /**
+  * @dev Creates a media and stores it in the contract.
+  * @dev A hash of the media is used as an unique key.
+  */
+    function _createMedia(
+        string _periodID,
+        string _mediaPresentationDuration,
+        string _maxSegmentDuration,
+        string _minBufferTime,
+        string _profiles,
+        string _baseURL
+    )
     private
   {
-    // Create the new period by the provided values.
-    Period memory period = Period({
-        baseUrl: _baseUrl,
-        duration: _duration
-    });
+      // Create the new media by the provided values.
+        Media memory media = Media({
+            periodID: _periodID,
+            mediaPresentationDuration: _mediaPresentationDuration,
+            maxSegmentDuration: _maxSegmentDuration,
+            minBufferTime: _minBufferTime,
+            profiles: _profiles,
+            baseURL: _baseURL,
+            numAdaptionSets: 0,
+            nonce: mediaNonce
 
-    // Generate the unique hash to store the permit.
-    bytes32 periodHash = _getPeriodHash(
-        period.baseUrl,
-        period.duration
-    );
+        });
 
-    periodsMapping[periodHash] = period;
+      // Generate the unique hash to store the permit.
+        bytes32 mediaHash = _getMediaHash(
+            media.periodID,
+            media.mediaPresentationDuration,
+            media.maxSegmentDuration,
+            media.minBufferTime,
+            media.profiles,
+            media.baseURL,
+            media.nonce
+        );
+
+        mediasMapping[mediaHash] = media;
+
+        emit MediaCreated(
+            mediaHash,
+            _baseURL
+        );
+
+        // Increase the permit nonce to get continuously unique hash values.
+        mediaNonce = mediaNonce.add(1);
+    }
+
+  /**
+  * @dev Creates a AdaptionSet and stores it in the media .
+  * @dev A hash of the media is used as an unique key.
+  */
+    function _addAdaptionSetToMedia(
+        bytes32 _mediaHash,
+        string _mimeType,
+        uint _maxWidth,
+        uint _maxHeight,
+        string _maxFrameRate
+    ) private {
+        Media storage media = mediasMapping[_mediaHash];
+        media.adaptionSets[media.numAdaptionSets++] = AdaptionSet({
+            mimeType: _mimeType,
+            maxWidth: _maxWidth,
+            maxHeight: _maxHeight,
+            maxFrameRate: _maxFrameRate,
+            numRepresentation: 0
+        });
+    }
+
     
-    emit PeriodCreated(
-      periodHash
-    );
-
-  }
-
-  function getPeriod(bytes32 _periodHash)
-    public
-    view
-    returns (string, string)
-  {
-  
-    // Build a tuple with the attributes of the specimen object.
-    return (
-      periodsMapping[_periodHash].baseUrl,
-      periodsMapping[_periodHash].duration
-    );
-  }
-
-  function _createAdaptionSet(
-    bool _segmentAlignment,
-    uint _maxWidth,
-    uint _maxHeight,
-    uint _maxFrameRate,
-    string _par,
-    string _lang
-  )
-    private
-  {
-    AdaptionSet memory adaptions = AdaptionSet(
-      _segmentAlignment,
-      _maxWidth,
-      _maxHeight,
-      _maxFrameRate,
-      _par,
-      _lang
-    );
-
-    bytes32 adaptionHash = _getAdaptionHash(
-      adaptions.segmentAlignment,
-      adaptions.maxWidth,
-      adaptions.maxHeight,
-      adaptions.maxFrameRate,
-      adaptions.par,
-      adaptions.lang
-    );
-
-    adationsMapping[adaptionHash] = adaptions;
-
-    emit AdaptionSetCreated(
-      adaptionHash
-    );
-  }
-
-  function getAdaptionSet(bytes32 _adaptionHash)
-    public
-    view
-    returns (
-      bool,
-      uint,
-      uint,
-      uint,
-      string,
-      string
-      )
-    {
-  
-    // Build a tuple with the attributes of the specimen object.
-    return (
-      adationsMapping[_adaptionHash].segmentAlignment,
-      adationsMapping[_adaptionHash].maxWidth,
-      adationsMapping[_adaptionHash].maxHeight,
-      adationsMapping[_adaptionHash].maxFrameRate,
-      adationsMapping[_adaptionHash].par,
-      adationsMapping[_adaptionHash].lang
-    );
-  }
-  
-  function _createRepresentationSet(
-    string _mimeType,
-    string _codecs,
-    string _sar,
-    uint _startWithSAP,
-    uint _bandwidth,
-    uint _timescale,
-    uint _duration,
-    bytes32[] _SegmentURL,
-    uint _representationCount,
-    uint _SegmentURLCount
-  )
-    private
-  {
-    for (uint i = 0; i < _representationCount; i++) {
-      RepresentationSet memory representations = RepresentationSet({
-        mimeType: _mimeType,
-        codecs: _codecs,
-        sar: _sar,
-        startWithSAP: _startWithSAP,
-        bandwidth: _bandwidth,
-        timescale: _timescale,
-        duration: _duration,
-        SegmentURL: new bytes32[](_SegmentURLCount)
-      }
-      );
-
-      //add segmentsURL
-      for(uint t = 0; t < _SegmentURLCount; t++) {
-          representations.SegmentURL[t] = _SegmentURL[t];
-      }
-
-      bytes32 representationHash = _getRepresentationHash(
-        representations.mimeType,
-        representations.codecs,
-        representations.sar,
-        representations.startWithSAP,
-        representations.bandwidth,
-        representations.timescale,
-        representations.duration
-      );
-
-      representationsMapping[representationHash] = representations;
+    /**
+  * @dev Creates a Representation and stores it in the adaptionSet.
+  * @dev A hash of the media is used as an unique key.
+  */
+  /*
+    function _addRepresentationToAdaptionSet(
+        string _id,
+        uint _bandwidth,
+        string _codecs,
+        uint _width,
+        uint _height,
+        string _frameRate,
+        bytes32 _mediaHash,
+        uint _numAdaptionSets,
+        string _audioSamplingRate
+    ) private {
+        Media storage media = mediasMapping[_mediaHash];
+        uint number = media.adaptionSets[_numAdaptionSets].numRepresentation++;
+        media.adaptionSets[_numAdaptionSets].representationSets[number] = Representation({
+            id: _id,
+            bandwidth: _bandwidth,
+            codecs: _codecs,
+            width: _width,
+            height: _height,
+            frameRate: _frameRate,
+            audioSamplingRate: _audioSamplingRate
+        });
     }
-
-    emit RepresentationSetCreated(
-      representationHash
-    );
-  }
-
-  function getRepresentationSet(bytes32 _representationHash)
-    public
-    view
-    returns (
-      string,
-      string,
-      string,
-      uint,
-      uint,
-      uint,
-      bytes32[]
-    )
-  {
-  
-    // Build a tuple with the attributes of the specimen object.
-    return (
-      representationsMapping[_representationHash].mimeType,
-      representationsMapping[_representationHash].codecs,
-      representationsMapping[_representationHash].sar,
-      representationsMapping[_representationHash].bandwidth,
-      representationsMapping[_representationHash].timescale,
-      representationsMapping[_representationHash].duration,
-      representationsMapping[_representationHash].SegmentURL
-    );
-  }
-
+  */
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,29 +9,7 @@ module.exports = async deployer => {
     .deploy(MediaFactory)
     .then(() => MediaFactory.deployed())
     .then(instance => {
-      instance.createMedia("Puppy-preview.jpg", "Viona's Puppyworld");
-      instance.createPeriod(
-        "PT0H0M12.500S",
-        "ipfs://QmSnNCfxL7R1ei8Dfk82gsEPykBZjC683iovaxaMxuE1WW/"
-      );
-      instance.createAdaptionSet(true, 1280, 720, 24, "16:9", "und");
-      instance.createRepresentationSet(
-        "video/mp4",
-        "avc1.4d401f",
-        "1:1",
-        1,
-        2412988,
-        24000,
-        96000,
-        [
-          "segment_init.mp4",
-          "segment_1.m4s",
-          "segment_2.m4s",
-          "segment_3.m4s",
-          "segment_4.m4s"
-        ],
-        1,
-        5
-      );
+      
+      instance.createMedia("period", "asdasd", "asdasdss","asdasdasd", "asdasd", "asdasd")
     });
 };
